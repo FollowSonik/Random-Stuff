da7d91812548a6d84e8ce29c54aedc347ddd778e
FollowSonik <sonic_906@mail.ru>
2020-07-08 03:49:24 +0300

    Thirty Fourth Commit, pog!

---
+++ Digital-Clock/script.js
@@ -0,0 +1,39 @@
+function showTime() {
+  const date = new Date()
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  let secs = date.getSeconds()
+
+  const formatHours = convertFormat(hours)
+
+  hours = checkTime(hours)
+
+  hours = addZero(hours)
+  minutes = addZero(minutes)
+  secs = addZero(secs)
+
+  document.getElementById('clock').innerHTML = `${hours}:${minutes}:${secs} ${formatHours}`
+}
+
+function convertFormat(time) {
+  const format = 'AM'
+  if (time >= 12) format = 'PM'
+
+  return format
+}
+
+function checkTime(time) {
+  if (time > 12) time = time - 12
+  if (time === 0) time = 12
+
+  return time
+}
+
+function addZero(time) {
+  if (time < 10) time = `0${time}`
+
+  return time
+}
+
+showTime()
+setInterval(showTime, 1000)
