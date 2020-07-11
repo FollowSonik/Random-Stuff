28e1ccb8d25578f22effe9e4eec41a67f86be65f
FollowSonik <sonic_906@mail.ru>
2020-07-11 07:09:22 +0300

    Thirty Sixth Commit, pog!

---
+++ Navbar/script.js
@@ -0,0 +1,14 @@
+const navbarBtn = document.querySelector('.navbar__btn')
+const navbarLinks = document.querySelector('.navbar__links')
+
+navbarBtn.addEventListener('click', function() {
+  const value = navbarLinks.classList.contains('navbar__collapse')
+
+  if (value) {
+    navbarLinks.classList.remove('navbar__collapse')
+    navbarBtn.classList.remove('change')
+  } else {
+    navbarLinks.classList.add('navbar__collapse')
+    navbarBtn.classList.add('change')
+  }
+})
