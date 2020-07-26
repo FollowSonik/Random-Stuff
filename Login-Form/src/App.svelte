fcd557876e9349d74330b55292ad6577bd096d71
FollowSonik <followsonik@gmail.com>
2020-07-26 17:19:01 +0700

    Added JavaScript.

--- Login-Form/src/App.svelte
+++ Login-Form/src/App.svelte
@@ -1,5 +1,19 @@
 <script>
+  let strength = 0;
+  let validations = [];
 
+  function validatePassword(event) {
+    const password = event.target.value;
+
+    validations = [
+      password.length > 5,
+      password.search(/[A-Z]/) > -1,
+      password.search(/[0-9]/) > -1,
+      password.search(/[$&+,:;=?@#]/),
+    ];
+
+    strength = validations.reduce((acc, cur) => acc + cur);
+  }
 </script>
 
 <style>
@@ -74,6 +88,40 @@
   .field:focus-within .label,
   .input:not(:placeholder-shown) + .label {
     transform: scale(0.8) translateY(-5rem);
+  }
+
+  .strength {
+    display: flex;
+    height: 20px;
+    width: 100%;
+  }
+
+  .bar {
+    margin-right: 5px;
+    height: 100%;
+    width: 25%;
+    transition: box-shadow 500ms;
+    box-shadow: inset 0px 20px #1f1f1f;
+  }
+
+  .bar-show {
+    box-shadow: none;
+  }
+
+  .bar-1 {
+    background: linear-gradient(to right, red, orangered);
+  }
+
+  .bar-2 {
+    background: linear-gradient(to right, orangered, yellow);
+  }
+
+  .bar-3 {
+    background: linear-gradient(to right, yellow, yellowgreen);
+  }
+
+  .bar-4 {
+    background: linear-gradient(to right, yellowgreen, green);
   }
 </style>
 
