627077f4f222b190553779ef35e37f86f1504ccc
FollowSonik <followsonik@gmail.com>
2020-07-26 17:10:10 +0700

    Need to fix the border, padding or smth in the input, because there is too much space between the placeholder and dashed lines..

--- Login-Form/src/App.svelte
+++ Login-Form/src/App.svelte
@@ -10,7 +10,6 @@
 
   .field {
     width: 100%;
-    margin: 0 auto;
     position: relative;
     border-bottom: 2px dashed var(--text-color);
     margin: 4rem auto 1rem;
@@ -42,17 +41,63 @@
   .input:invalid {
     color: orangered;
   }
+
+  .field::after {
+    content: '';
+    position: relative;
+    display: block;
+    height: 4px;
+    width: 100%;
+    background: #d16dff;
+    transform: scaleX(0);
+    transform-origin: 0%;
+    transition: all 500ms ease;
+    top: 2px;
+  }
+
+  .field:focus-within {
+    border-color: transparent;
+  }
+
+  .field:focus-within::after {
+    transform: scaleX(1);
+  }
+
+  .label {
+    z-index: -1;
+    position: relative;
+    transform: translateY(-2rem);
+    transform-origin: 0%;
+    transition: transform 400ms;
+  }
+
+  .field:focus-within .label,
+  .input:not(:placeholder-shown) + .label {
+    transform: scale(0.8) translateY(-5rem);
+  }
 </style>
 
 <main>
   <form>
     <div class="field">
-      <input type="email" name="email" class="input" placeholder="" />
+      <input type="email" name="email" class="input" placeholder=" " />
       <label for="email" class="label">Email</label>
     </div>
     <div class="field">
-      <input type="password" name="email" class="input" placeholder="" />
+      <input type="password" name="email" class="input" placeholder=" " />
       <label for="password" class="label">Password</label>
     </div>
+    <div class="strength">
+      <span class="bar bar-1" />
+      <span class="bar bar-2" />
+      <span class="bar bar-3" />
+      <span class="bar bar-4" />
+    </div>
+    <ul>
+      <li>Must be at least 5 characters</li>
+      <li>Must contain a capital letter</li>
+      <li>Must contain a number</li>
+      <li>Must contain one of $&+,:;=?@#</li>
+    </ul>
   </form>
 </main>
