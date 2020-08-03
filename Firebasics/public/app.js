a850b6dd948e89d6ebe63f4415fbaec4dc4728a8
FollowSonik <followsonik@gmail.com>
2020-08-03 16:21:34 +0700

    It availbale to store the JWT via Sign in with Google, pog!

--- Firebasics/public/app.js
+++ Firebasics/public/app.js
@@ -1,1 +1,15 @@
-console.log(firebase);
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
