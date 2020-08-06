1817528221bc35b03c24e19d424c86a159bfcfd5
FollowSonik <followsonik@gmail.com>
2020-08-06 15:29:08 +0700

    Styles..

--- ML AI With Neural Network/script.js
+++ ML AI With Neural Network/script.js
@@ -1,23 +1,19 @@
 const net = new brain.NeuralNetwork();
-const diagram = document.getElementById('diagram');
 
-net.train([
+const data = [
   {
-    input: [0, 0],
+    input: { r: 0, g: 0, b: 0, },
+    output: [1],
+  },
+  {
+    input: { r: 1, g: 1, b: 1, },
     output: [0],
   },
-  {
-    input: [1, 0],
-    output: [1],
-  },
-  {
-    input: [0, 1],
-    output: [1],
-  },
-  {
-    input: [1, 1],
-    output: [1],
-  },
-]);
+];
 
+net.train(data);
+
+const diagram = document.getElementById('diagram');
 diagram.innerHTML = brain.utilities.toSVG(net);
+
+console.log(net.run({ r: 1, g: .5, b: 0 }));
