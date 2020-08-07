fc3d8ed5dd8abd18dc34f50f708226571ba35556
FollowSonik <followsonik@gmail.com>
2020-08-07 21:42:21 +0700

    Setup to make an infinite scroll with React.

---
+++ infinite-scroll/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <input type="text"></input>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading..</div>
+      <div>Error</div>
+    </>
+  );
+}
