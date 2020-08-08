15db2d0c87aa1696793b094b2e55810f39e893e1
FollowSonik <followsonik@gmail.com>
2020-08-08 12:19:10 +0700

    lastBookElementRef, useCallback.

--- infinite-scroll/src/App.js
+++ infinite-scroll/src/App.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import useBookSearch from './useBookSearch';
 
 export default function App() {
   const [query, setQuery] = useState();
   const [pageNumber, setPageNumber] = useState(1);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(node => {
+    console.log(node);
+  });
 
   function handleSearch(event) {
     setQuery(event.target.value);
@@ -14,9 +19,13 @@ export default function App() {
 
   return (
     <>
-      <input type="text" onChange={handleSearch}></input>
-      {books.map(book => {
-        return <div key={book}>{book}</div>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        } else {
+          return <div key={book}>{book}</div>
+        }
       })}
       <div>{loading && 'Loading..'}</div>
       <div>{error && 'Error'}</div>
