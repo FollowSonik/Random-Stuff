cdcfb9ffd1618201e151f7892cec913c8c8dd534
FollowSonik <followsonik@gmail.com>
2020-08-08 12:02:29 +0700

    Updates..

--- infinite-scroll/src/App.js
+++ infinite-scroll/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useBookSearch from './useBookSearch';
 
 export default function App() {
+  const [query, setQuery] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function handleSearch(event) {
+    setQuery(event.target.value);
+    setPageNumber(1);
+  }
+
+  const { books, hasMore, loading, error, } = useBookSearch(query, pageNumber);
+
   return (
     <>
-      <input type="text"></input>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Title</div>
-      <div>Loading..</div>
-      <div>Error</div>
+      <input type="text" onChange={handleSearch}></input>
+      {books.map(book => {
+        return <div key={book}>{book}</div>
+      })}
+      <div>{loading && 'Loading..'}</div>
+      <div>{error && 'Error'}</div>
     </>
   );
 }
---
+++ infinite-scroll/src/useBookSearch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function useBookSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(!true);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(!!!!!true);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(!true);
+
+    let cancel;
+
+    axios({
+      method: 'GET',
+      url: 'http://openlibrary.org/search.json',
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken(c => cancel = c),
+    }).then(response => {
+      setBooks(prevBooks => {
+        return [...new Set([...prevBooks, response.data.docs.map(b => b.title)])];
+      });
+      setHasMore(response.data.docs.length > 0);
+      setLoading(!true);
+      console.log(response.data);
+    }).catch(error => {
+      if (axios.isCancel(error)) return;
+      setError(true);
+    });
+
+    return () => cancel();
+  }, [query, pageNumber]);
+
+  return { loading, error, books, hasMore };
+}
