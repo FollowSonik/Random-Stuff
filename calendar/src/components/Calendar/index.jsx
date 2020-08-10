bb5abfc9ecee7b31281d72fa6f2b3712e82af027
FollowSonik <followsonik@gmail.com>
2020-08-10 13:34:36 +0700

    Updates..

--- calendar/src/components/Calendar/index.jsx
+++ calendar/src/components/Calendar/index.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import './index.css';
 
-export default function () {
-  return (
-    <div className="calendar">
-      <header>
-        <button>{'<'}</button>
-        <select></select>
-        <select></select>
-        <button>{'>'}</button>
-      </header>
-      <table>
-        <thead>
-          <tr></tr>
-        </thead>
-        <tbody></tbody>
-      </table>
-    </div>
-  );
+export default class Calendar extends React.Component {
+  static defaultProps = {
+    years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+    monthNames: ['Январь', 'Ферваль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    weekDayNames: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+  };
+
+  render() {
+    const { years, monthNames, weekDayNames } = this.props;
+
+    return (
+      <div className="calendar">
+        <header>
+          <button>{'<'}</button>
+          <select>
+            {monthNames.map((name, index) => {
+              return <option key={name} value={index}>{name}</option>
+            })}
+          </select>
+          <select></select>
+          <button>{'>'}</button>
+        </header>
+        <table>
+          <thead>
+            <tr></tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    );
+  }
 }
