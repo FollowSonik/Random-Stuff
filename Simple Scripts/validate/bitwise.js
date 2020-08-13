7af567658c5313a373289194066ea922db452369
FollowSonik <followsonik@gmail.com>
2020-08-14 04:00:19 +0700

    Bitwise validate, pog!

---
+++ Simple Scripts/validate/bitwise.js
@@ -0,0 +1,12 @@
+function validate(n) {
+  n = n.toString().split('').map(Number).reverse();
+
+  return n.reduce((sum, digit, index) => {
+    if (index & 1) digit <<= 1;
+    if (digit > 9) digit -= 9;
+
+    return sum + digit;
+  }, 0) % 10 == 0;
+}
+
+console.log(validate(1234567));
