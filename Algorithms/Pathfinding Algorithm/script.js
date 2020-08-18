5f1314d74ccba1ec077748fbef73f2b4d324b96e
FollowSonik <followsonik@gmail.com>
2020-08-18 12:09:28 +0700

    Updates..

--- Algorithms/Pathfinding Algorithm/script.js
+++ Algorithms/Pathfinding Algorithm/script.js
@@ -2,17 +2,72 @@ const cols = 5;
 const rows = 5;
 const grid = new Array(cols);
 
+const openSet = [];
+const closedSet = [];
+
+let start;
+let end;
+let v, h;
+
+function Spot(i, j) {
+  this.x = i;
+  this.y = j;
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+
+  this.show = function (color) {
+    fill(color);
+    noStroke();
+    rect(this.x * w, this.y * h, w - 1, h - 1);
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
   console.log('A*');
+
+  w = width / cols;
+  h = height / rows;
 
   for (let i = 0; i < cols; i++) {
     grid[i] = new Array(rows);
   }
 
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+
+  openSet.push(start);
+
   console.log(grid);
 }
 
 function draw() {
+  if (openSet.length > 0) {
+
+  } else {
+
+  }
+
   background(111);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < cols; j++) {
+      grid[i][j].show(color(255));
+    }
+  }
+
+  for (let i = 0; i < closedSet.length; i++) {
+    closedSet[i].show(color(255, 0, 0));
+  }
+
+  for (let i = 0; i < openSet.length; i++) {
+    openSet[i].show(color(0, 255, 0));
+  }
 }
