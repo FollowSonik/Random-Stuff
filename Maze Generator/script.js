74db4ae936ff8c0ef06f7ef4d930721f1bb0c084
FollowSonik <followsonik@gmail.com>
2020-08-21 10:43:28 +0700

    Array with Boolean variables\values..

--- Maze Generator/script.js
+++ Maze Generator/script.js
@@ -27,16 +27,29 @@ function draw() {
 function Cell(i, j) {
   this.i = i;
   this.j = j;
+  this.walls = [true, true, true, true];
 
   this.show = function () {
     let x = this.i * w;
     let y = this.j * w;
 
     stroke(255);
-    line(x, y, x + w, y);
-    // line(x + w, y, x + w, y + w);
-    line(x + w, y + w, x, y + w);
-    line(x, y + w, x, y);
+
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+
+    if (this.walls[1]) {
+      // line(x + w, y, x + w, y + w);
+    }
+
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
 
     // noFill();
     // rect(x, y, w, w);
