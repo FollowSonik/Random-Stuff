f87b7e6136abbe484e4d8e56231f453e8ae1e0dc
FollowSonik <followsonik@gmail.com>
2020-08-25 17:47:24 +0700

    Instead of hardcoding numbers, pick the random nums.

---
+++ Algorithms/Binary Search Tree/Tree.js
@@ -0,0 +1,17 @@
+function Tree() {
+  this.root = null;
+}
+
+Tree.prototype.traverse = function () {
+  this.root.visit();
+}
+
+Tree.prototype.addValue = function (val) {
+  let n = new Node(val);
+
+  if (this.root == null) {
+    this.root = n;
+  } else {
+    this.root.addNode(n);
+  }
+};
--- Algorithms/Binary Search Tree/script.js
+++ Algorithms/Binary Search Tree/script.js
@@ -4,41 +4,11 @@ function setup() {
   noCanvas();
 
   tree = new Tree();
-  tree.addValue(5);
-  tree.addValue(3);
-  tree.addValue(7);
-  tree.addValue(6);
+
+  for (let i = 0; i < 10; i++) {
+    tree.addValue(floor(random(0, 100)));
+  }
+
   console.log(tree);
-}
-
-function Tree() {
-  this.root = null;
-}
-
-Tree.prototype.addValue = function (val) {
-  let n = new Node(val);
-
-  if (this.root == null) {
-    this.root = n;
-  } else {
-    this.root.addNode(n);
-  }
-};
-
-Node.prototype.addNode = function (n) {
-  if (n.value < this.value) {
-    if (this.left == null) {
-      this.left = n;
-    } else this.left.addNode(n);
-  } else if (n.value > this.value) {
-    if (this.right == null) {
-      this.right = n;
-    } else this.right.addNode(n);
-  }
-};
-
-function Node(val) {
-  this.value = val;
-  this.left = null;
-  this.right = null;
+  tree.traverse();
 }
