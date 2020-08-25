d04475496c23df37a3dd8668982ca9f0d7502562
FollowSonik <followsonik@gmail.com>
2020-08-25 17:37:55 +0700

    It looks like a tree, pog!

--- Algorithms/Binary Search Tree/script.js
+++ Algorithms/Binary Search Tree/script.js
@@ -4,20 +4,34 @@ function setup() {
   noCanvas();
 
   tree = new Tree();
-  let n = new Node(5);
-
-  tree.addNode(n);
-
+  tree.addValue(5);
   console.log(tree);
+  tree.addValue(3);
 }
 
 function Tree() {
   this.root = null;
 }
 
-Tree.prototype.addNode = n => {
+Tree.prototype.addValue = function (val) {
+  let n = new Node(val);
+
   if (this.root == null) {
     this.root = n;
+  } else {
+    this.root.addNode(n);
+  }
+};
+
+Node.prototype.addNode = function (n) {
+  if (n.value < this.value) {
+    if (this.left == null) {
+      this.left = n;
+    } else this.left.addNode(n);
+  } else if (n.value > this.value) {
+    if (this.right == null) {
+      this.right = n;
+    } else this.right.addNode(n);
   }
 };
 
