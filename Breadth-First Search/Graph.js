3ed0dfc20740bb84b0790119121bbba652aa64c3
FollowSonik <followsonik@gmail.com>
2020-08-27 15:15:51 +0700

    Updates..

--- Breadth-First Search/Graph.js
+++ Breadth-First Search/Graph.js
@@ -2,3 +2,9 @@ function Graph() {
   this.nodes = [];
   this.graph = {};
 }
+
+Graph.prototype.addNode = function (n) {
+  this.nodes.push(n);
+  const title = n.value;
+  this.graph[title] = n;
+}
--- Breadth-First Search/script.js
+++ Breadth-First Search/script.js
@@ -1,10 +1,27 @@
 let data;
+let graph;
 
 function preload() {
   data = loadJSON("kevin.json");
 }
 
 function setup() {
+  graph = new Graph();
   noCanvas();
-  console.log(data);
+  const movies = data.movies;
+
+  for (let i = 0; i < movies.length; i++) {
+    const movie = movies[i].title;
+    const cast = movies[i].cast;
+    const n = new Node(movie);
+    graph.addNode(n);
+
+    for (let j = 0; j < cast.length; j++) {
+      const actor = cast[j];
+      const actorNode = new Node(actor);
+      graph.addNode(actorNode);
+    }
+  }
+
+  console.log(graph);
 }
