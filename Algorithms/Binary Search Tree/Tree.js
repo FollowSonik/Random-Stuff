2ef5fe086a52d23fba9c1dce225ce1bca9737f65
FollowSonik <followsonik@gmail.com>
2020-08-28 21:27:18 +0700

    functions to Classes

--- Algorithms/Binary Search Tree/Tree.js
+++ Algorithms/Binary Search Tree/Tree.js
@@ -1,23 +1,26 @@
-function Tree() {
-  this.root = null;
-}
-
-Tree.prototype.traverse = function () {
-  this.root.visit(this.root);
-};
-
-Tree.prototype.search = function (val) {
-  return this.root.search(val);
-};
-
-Tree.prototype.addValue = function (val) {
-  let n = new Node(val);
-
-  if (this.root == null) {
-    this.root = n;
-    this.root.x = width / 2;
-    this.root.y = 16;
-  } else {
-    this.root.addNode(n);
+class Tree {
+  constructor() {
+    this.root = null;
   }
-};
+
+  traverse() {
+    this.root.visit(this.root);
+  }
+
+  search(val) {
+    return this.root.search(val);
+  }
+
+  addValue(val) {
+    let n = new Node(val);
+
+    if (this.root == null) {
+      this.root = n;
+      this.root.x = width / 2;
+      this.root.y = 16;
+    }
+    else {
+      this.root.addNode(n);
+    }
+  }
+}
