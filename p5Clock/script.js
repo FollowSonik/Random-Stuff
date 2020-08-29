56945ff6330d47a8ca3753abfb0ec3b7d85a4a75
FollowSonik <followsonik@gmail.com>
2020-08-29 18:06:33 +0700

    Moving the mouse drawing an arc.

--- p5Clock/script.js
+++ p5Clock/script.js
@@ -1,5 +1,6 @@
 function setup() {
   createCanvas(400, 400);
+  angleMode(DEGREES);
 }
 
 function draw() {
@@ -9,7 +10,19 @@ function draw() {
   let min = minute();
   let secs = second();
 
-  fill(255);
-  noStroke();
-  text(`${h}:${min}:${secs}`, 10, 200);
+  // strokeWeight(4);
+  // stroke(255);
+  // noFill();
+  // ellipse(200, 200, 300, 300);
+
+  strokeWeight(8);
+  stroke(255, 100, 150);
+
+  let end = map(mouseX, 0, width, 0, 360);
+
+  arc(200, 200, 300, 300, 0, end);
+
+  // fill(255);
+  // noStroke();
+  // text(`${h}:${min}:${secs}`, 10, 200);
 }
