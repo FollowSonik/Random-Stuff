ef9961a3d1a4612c48559a117de5e0fd38cc0172
FollowSonik <followsonik@gmail.com>
2020-08-30 21:52:47 +0700

    Kinda monkas..

--- Smart Rockets/scripts.js
+++ Smart Rockets/scripts.js
@@ -1,5 +1,6 @@
 let rocket;
 let population;
+let lifespan = 200;
 
 function setup() {
   createCanvas(400, 300);
@@ -28,16 +29,28 @@ function Population() {
   }
 }
 
+function DNA() {
+  this.genes = [];
+
+  for (let i = 0; i < lifespan; i++) {
+    this.genes[i] = p5.Vector.random2D();
+  }
+}
+
 function Rocket() {
   this.pos = createVector(width / 2, height);
-  this.vel = p5.Vector.random2D();
+  this.vel = createVector();
   this.acc = createVector();
+  this.dna = new DNA();
+  this.count = 0;
 
   this.applyForce = function (force) {
     this.acc.add(force);
   }
 
   this.update = function () {
+    this.applyForce(this.dna.genes[this.count]);
+    this.count++;
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
