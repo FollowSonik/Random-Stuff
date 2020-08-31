884c0aed1ee8b91b98ae455279175f8068d5d16b
FollowSonik <followsonik@gmail.com>
2020-08-31 18:18:15 +0700

    Target.

--- Smart Rockets/scripts.js
+++ Smart Rockets/scripts.js
@@ -3,19 +3,29 @@ let population;
 let lifespan = 200;
 let lifeP;
 let count = 0;
+let target;
 
 function setup() {
   createCanvas(400, 300);
   rocket = new Rocket();
   population = new Population();
   lifeP = createP();
+  target = createVector(width / 2, 50);
 }
 
 function draw() {
   background(0);
   population.run();
   lifeP.html(count);
+
   count++;
+
+  if (count == lifespan) {
+    population = new Population();
+    count = 0;
+  }
+
+  ellipse(target.x, target.y, 16, 16);
 }
 
 function Population() {
