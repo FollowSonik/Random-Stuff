5bd822ff73e9abef5ababae881ade2d071bebe2b
FollowSonik <followsonik@gmail.com>
2020-09-01 21:34:30 +0700

    Updates..

--- Smart Rockets/scripts.js
+++ Smart Rockets/scripts.js
@@ -118,6 +118,7 @@ function Rocket(dna) {
   this.pos = createVector(width / 2, height);
   this.vel = createVector();
   this.acc = createVector();
+  this.completed = false;
 
   if (dna) {
     this.dna = dna;
@@ -138,11 +139,20 @@ function Rocket(dna) {
   }
 
   this.update = function () {
+    const d = dist(this.pos.x, this.pos.y, target.x, target.y);
+
+    if (d < 10) {
+      this.completed = true;
+      this.pos = target.copy();
+    }
+
     this.applyForce(this.dna.genes[count]);
 
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
+    if !(this.completed) {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    }
   }
 
   this.show = function () {
