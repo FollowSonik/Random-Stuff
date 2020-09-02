686d305e88ee19d383efc6de97528a84689d0bb9
FollowSonik <followsonik@gmail.com>
2020-09-02 22:39:54 +0700

    Converted all constructors to Class

--- Smart Rockets/scripts.js
+++ Smart Rockets/scripts.js
@@ -1,6 +1,6 @@
 let rocket;
 let population;
-let lifespan = 200;
+let lifespan = 400;
 let lifeP;
 let count = 0;
 let target;
@@ -8,6 +8,7 @@ let rx = 100;
 let ry = 150;
 let rw = 200;
 let rh = 10;
+let maxForse = .2;
 
 function setup() {
   createCanvas(400, 300);
@@ -38,162 +39,182 @@ function draw() {
   ellipse(target.x, target.y, 16, 16);
 }
 
-function Population() {
-  this.rockets = [];
-  this.popsize = 25;
-  this.matingpool = [];
-
-  for (let i = 0; i < this.popsize; i++) {
-    this.rockets[i] = new Rocket();
-  }
-
-  this.evaluate = function () {
-    let maxfit = 0;
+class Population {
+  constructor() {
+    this.rockets = [];
+    this.popsize = 25;
+    this.matingpool = [];
 
     for (let i = 0; i < this.popsize; i++) {
-      this.rockets[i].calcFitness();
+      this.rockets[i] = new Rocket();
+    }
 
-      if (this.rockets[i].fitness > maxfit) {
-        maxfit = this.rockets[i].fitness;
+    this.evaluate = function () {
+      let maxfit = 0;
+
+      for (let i = 0; i < this.popsize; i++) {
+        this.rockets[i].calcFitness();
+
+        if (this.rockets[i].fitness > maxfit) {
+          maxfit = this.rockets[i].fitness;
+        }
       }
-    }
 
-    for (let i = 0; i < this.popsize; i++) {
-      this.rockets[i].fitness /= maxfit;
-    }
-
-    this.matingpool.length = 0;
-
-    for (let i = 0; i < this.popsize; i++) {
-      const n = this.rockets[i].fitness * 100;
-
-      for (let j = 0; j < n; j++) {
-        this.matingpool.push(this.rockets[i]);
+      for (let i = 0; i < this.popsize; i++) {
+        this.rockets[i].fitness /= maxfit;
       }
-    }
-  }
 
-  this.selection = function () {
-    const newRockets = [];
+      this.matingpool.length = 0;
 
-    for (let i = 0; i < this.rockets.length; i++) {
-      const parentA = random(this.matingpool).dna;
-      const parentB = random(this.matingpool).dna;
-      const child = parentA.crossover(parentB);
+      for (let i = 0; i < this.popsize; i++) {
+        const n = this.rockets[i].fitness * 100;
 
-      child.mutation();
+        for (let j = 0; j < n; j++) {
+          this.matingpool.push(this.rockets[i]);
+        }
+      }
+    };
 
-      newRockets[i] = new Rocket(child);
-    }
+    this.selection = function () {
+      const newRockets = [];
 
-    this.rockets = newRockets;
-  }
+      for (let i = 0; i < this.rockets.length; i++) {
+        const parentA = random(this.matingpool).dna;
+        const parentB = random(this.matingpool).dna;
+        const child = parentA.crossover(parentB);
 
-  this.run = function () {
-    for (let i = 0; i < this.popsize; i++) {
-      this.rockets[i].update();
-      this.rockets[i].show();
-    }
+        child.mutation();
+
+        newRockets[i] = new Rocket(child);
+      }
+
+      this.rockets = newRockets;
+    };
+
+    this.run = function () {
+      for (let i = 0; i < this.popsize; i++) {
+        this.rockets[i].update();
+        this.rockets[i].show();
+      }
+    };
   }
 }
 
-function DNA(genes) {
-  if (genes) {
-    this.genes = genes;
-  } else {
-    this.genes = [];
-
-    for (let i = 0; i < lifespan; i++) {
-      this.genes[i] = p5.Vector.random2D();
-      this.genes[i].setMag(.1);
+class DNA {
+  constructor(genes) {
+    if (genes) {
+      this.genes = genes;
     }
-  }
+    else {
+      this.genes = [];
 
-  this.crossover = function (partner) {
-    const newGenes = [];
-
-    const mid = floor(random(this.genes.length));
-
-    for (let i = 0; i < this.genes.length; i++) {
-      if (i > mid) newGenes[i] = this.genes[i];
-      else newGenes[i] = partner.genes[i];
-    }
-
-    return new DNA(newGenes);
-  }
-
-  this.mutation = function () {
-    for (let i = 0; i < this.genes.length; i++) {
-      if (random(1) < .01) {
+      for (let i = 0; i < lifespan; i++) {
         this.genes[i] = p5.Vector.random2D();
-        this.genes[i].setMag(.1);
+        this.genes[i].setMag(maxForse);
       }
     }
+
+    this.crossover = function (partner) {
+      const newGenes = [];
+
+      const mid = floor(random(this.genes.length));
+
+      for (let i = 0; i < this.genes.length; i++) {
+        if (i > mid)
+          newGenes[i] = this.genes[i];
+        else
+          newGenes[i] = partner.genes[i];
+      }
+
+      return new DNA(newGenes);
+    };
+
+    this.mutation = function () {
+      for (let i = 0; i < this.genes.length; i++) {
+        if (random(1) < .01) {
+          this.genes[i] = p5.Vector.random2D();
+          this.genes[i].setMag(maxForse);
+        }
+      }
+    };
   }
 }
 
-function Rocket(dna) {
-  this.pos = createVector(width / 2, height);
-  this.vel = createVector();
-  this.acc = createVector();
-  this.completed = false;
-  this.crashed = false;
 
-  if (dna) {
-    this.dna = dna;
-  } else {
-    this.dna = new DNA();
-  }
+class Rocket {
+  constructor(dna) {
+    this.pos = createVector(width / 2, height);
+    this.vel = createVector();
+    this.acc = createVector();
+    this.completed = false;
+    this.crashed = false;
 
-  this.fitness = 0;
-
-  this.applyForce = function (force) {
-    this.acc.add(force);
-  }
-
-  this.calcFitness = function () {
-    const d = dist(this.pos.x, this.pos.y, target.x, target.y);
-
-    this.fitness = map(d, 0, width, width, 0);
-
-    if (this.completed) {
-      this.fitness *= 10;
+    if (dna) {
+      this.dna = dna;
+    }
+    else {
+      this.dna = new DNA();
     }
 
-    if (this.crashed) {
-      this.fitness = 1;
-    }
-  }
+    this.fitness = 0;
 
-  this.update = function () {
-    const d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    this.applyForce = function (force) {
+      this.acc.add(force);
+    };
 
-    if (d < 10) {
-      this.completed = true;
-      this.pos = target.copy();
-    }
+    this.calcFitness = function () {
+      const d = dist(this.pos.x, this.pos.y, target.x, target.y);
 
-    if (this.pos.x > rx && this.pos.x < rx + rw && this.pos.y > ry && this.pos.y < ry + rh) {
-      this.crashed = true;
-    }
+      this.fitness = map(d, 0, width, width, 0);
 
-    this.applyForce(this.dna.genes[count]);
+      if (this.completed) {
+        this.fitness *= 10;
+      }
 
-    if (!this.completed && !this.crashed) {
-      this.vel.add(this.acc);
-      this.pos.add(this.vel);
-      this.acc.mult(0);
-    }
-  }
+      if (this.crashed) {
+        this.fitness /= 10;
+      }
+    };
 
-  this.show = function () {
-    push();
-    noStroke();
-    fill(250, 100);
-    translate(this.pos.x, this.pos.y);
-    rotate(this.vel.heading());
-    rectMode(CENTER);
-    rect(0, 0, 25, 5);
-    pop();
+    this.update = function () {
+      const d = dist(this.pos.x, this.pos.y, target.x, target.y);
+
+      if (d < 10) {
+        this.completed = true;
+        this.pos = target.copy();
+      }
+
+      if (this.pos.x > rx && this.pos.x < rx + rw && this.pos.y > ry && this.pos.y < ry + rh) {
+        this.crashed = true;
+      }
+
+      if (this.pos.x > width || this.pos.x < 0) {
+        this.crashed = true;
+      }
+
+      if (this.pos.y > height || this.pos.y < 0) {
+        this.crashed = true;
+      }
+
+      this.applyForce(this.dna.genes[count]);
+
+      if (!this.completed && !this.crashed) {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+        this.vel.limit(4);
+      }
+    };
+
+    this.show = function () {
+      push();
+      noStroke();
+      fill(250, 100);
+      translate(this.pos.x, this.pos.y);
+      rotate(this.vel.heading());
+      rectMode(CENTER);
+      rect(0, 0, 25, 5);
+      pop();
+    };
   }
 }
