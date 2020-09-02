d203ede5752a67222137c51850199f9281343c76
FollowSonik <followsonik@gmail.com>
2020-09-02 22:44:07 +0700

    Methods

--- Smart Rockets/scripts.js
+++ Smart Rockets/scripts.js
@@ -48,56 +48,56 @@ class Population {
     for (let i = 0; i < this.popsize; i++) {
       this.rockets[i] = new Rocket();
     }
-
-    this.evaluate = function () {
-      let maxfit = 0;
-
-      for (let i = 0; i < this.popsize; i++) {
-        this.rockets[i].calcFitness();
-
-        if (this.rockets[i].fitness > maxfit) {
-          maxfit = this.rockets[i].fitness;
-        }
-      }
-
-      for (let i = 0; i < this.popsize; i++) {
-        this.rockets[i].fitness /= maxfit;
-      }
-
-      this.matingpool.length = 0;
-
-      for (let i = 0; i < this.popsize; i++) {
-        const n = this.rockets[i].fitness * 100;
-
-        for (let j = 0; j < n; j++) {
-          this.matingpool.push(this.rockets[i]);
-        }
-      }
-    };
-
-    this.selection = function () {
-      const newRockets = [];
-
-      for (let i = 0; i < this.rockets.length; i++) {
-        const parentA = random(this.matingpool).dna;
-        const parentB = random(this.matingpool).dna;
-        const child = parentA.crossover(parentB);
-
-        child.mutation();
-
-        newRockets[i] = new Rocket(child);
-      }
-
-      this.rockets = newRockets;
-    };
-
-    this.run = function () {
-      for (let i = 0; i < this.popsize; i++) {
-        this.rockets[i].update();
-        this.rockets[i].show();
-      }
-    };
   }
+
+  evaluate() {
+    let maxfit = 0;
+
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].calcFitness();
+
+      if (this.rockets[i].fitness > maxfit) {
+        maxfit = this.rockets[i].fitness;
+      }
+    }
+
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].fitness /= maxfit;
+    }
+
+    this.matingpool.length = 0;
+
+    for (let i = 0; i < this.popsize; i++) {
+      const n = this.rockets[i].fitness * 100;
+
+      for (let j = 0; j < n; j++) {
+        this.matingpool.push(this.rockets[i]);
+      }
+    }
+  };
+
+  selection() {
+    const newRockets = [];
+
+    for (let i = 0; i < this.rockets.length; i++) {
+      const parentA = random(this.matingpool).dna;
+      const parentB = random(this.matingpool).dna;
+      const child = parentA.crossover(parentB);
+
+      child.mutation();
+
+      newRockets[i] = new Rocket(child);
+    }
+
+    this.rockets = newRockets;
+  };
+
+  run() {
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].update();
+      this.rockets[i].show();
+    }
+  };
 }
 
 class DNA {
